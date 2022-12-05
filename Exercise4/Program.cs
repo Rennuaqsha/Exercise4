c7e3841ec342b854cacec7db71c2addd0354f9af
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-12-05 17:05:59 +0800

    create class node

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -3,5 +3,14 @@
 
 namespace Exercise_4
 {
-    
+    class node
+    {
+        public int info;
+        public node next;
+        public node(int i, node n)
+        {
+            info = i;
+            next = n;
+        }
+    }
 }
