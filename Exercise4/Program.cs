43001be197456eceebeaa72378ff4eb36bd25074
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-12-05 17:43:30 +0800

    create method void pop for determine the popped elements

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -8,6 +8,7 @@ namespace Exercise_4
         public int info;
         public node next;
         private int[] rennu = new int[68];
+        int top;
         public node(int i, node n)
         {
             info = i;
@@ -27,7 +28,11 @@ namespace Exercise_4
         {
             return top == null;
         }
-            
 
+        public void pop()
+        {
+            Console.WriteLine("\n the poped elements is: " + top.info);
+            top = top.next; // make top point to the next node is squence
+        }
     }
 }
