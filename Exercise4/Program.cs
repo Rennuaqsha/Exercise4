6ca4c4083a90aa3c4bf795a61da779932c7a5fa7
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-12-05 17:19:08 +0800

    create class stack

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -7,10 +7,16 @@ namespace Exercise_4
     {
         public int info;
         public node next;
+        private int[] rennu = new int[68];
         public node(int i, node n)
         {
             info = i;
             next = n;
         }
+    }
+
+    class stack
+    {
+
     }
 }
